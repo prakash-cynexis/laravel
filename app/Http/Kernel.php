587fc4ede6df987cf6649b646cb0c42bb53db177
Prakash <prakash@cynexis.com>
2019-07-05 14:58:51 +0530

    add request response middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Http;
 use App\Http\Middleware\Authenticate;
 use App\Http\Middleware\CheckForMaintenanceMode;
 use App\Http\Middleware\EncryptCookies;
+use App\Http\Middleware\LogRequestResponse;
 use App\Http\Middleware\RedirectIfAuthenticated;
 use App\Http\Middleware\TrimStrings;
 use App\Http\Middleware\TrustProxies;
@@ -54,6 +55,7 @@ class Kernel extends HttpKernel {
             ShareErrorsFromSession::class,
             VerifyCsrfToken::class,
             SubstituteBindings::class,
+            LogRequestResponse::class
         ],
 
         'api' => [
